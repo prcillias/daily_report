e3f37480116b3f6a65390ecf6d17c478a6c1bfbe
prcillias <pricilliasilfany22@gmail.com>
2024-04-18 08:30:01 +0700

    index.html

---
+++ app.py
@@ -0,0 +1,151 @@
+from flask import Flask, render_template, request, jsonify, send_file
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import Table
+from datetime import datetime
+import pandas as pd
+import os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database', 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class CustTemp(db.Model):
+    date = db.Column(db.String(10))
+    nama = db.Column(db.String(255), primary_key=True)
+    average_temp = db.Column(db.Float)
+    safe_percentage = db.Column(db.Float)
+    
+    def __repr__(self):
+        return f'<{self.nama}>'
+
+@app.route('/')
+def home():
+    data = CustTemp.query.all()
+    return render_template('index.html', data=data)
+
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    date = request.args.get('date')
+
+    data = CustTemp.query.filter(CustTemp.date == date).all()
+    formatted_data = []
+    if data:
+        for item in data:
+            formatted_item = {
+                'nama': item.nama,
+                'average_temp': item.average_temp,
+                'safe_percentage': item.safe_percentage
+            }
+            formatted_data.append(formatted_item)
+        return jsonify({"message": "success", "data": formatted_data})
+    else:
+        return jsonify({"message": "no data"})
+    
+@app.route('/clear-data', methods=['GET'])
+def clear_data():
+    date = request.args.get('date')
+    data = CustTemp.query.filter(CustTemp.date == date).all()
+    if data:
+        for d in data:
+            db.session.delete(d)
+        db.session.commit()
+        return jsonify({"message": "success"})
+    else:
+        return jsonify({"message": "no data"}) 
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    nama = request.form.get('selectedCompany')
+    date = request.form.get('date')
+    uploaded_files = request.files.getlist('excel_files')
+    data = []
+
+    existing_data = CustTemp.query.filter_by(nama=nama).first()
+    if existing_data:
+        return jsonify({"message": "existed"})
+    
+    for index, excel_file in enumerate(uploaded_files):
+        if excel_file.filename != '':
+            df = pd.read_excel(excel_file)
+            df.rename(columns={df.columns[-1]: "Status"}, inplace=True)
+            data.append(df)
+            
+    if data:
+        all_data = pd.concat(data, ignore_index=True)
+        average_oil_temp = round(all_data['OilTemp'].mean(), 2)
+        value_counts = all_data['Status'].value_counts()
+        safe_percentage = round((value_counts.get("Safe", 0) / len(all_data)) * 100, 2)
+        new = CustTemp(date=date, nama=nama, average_temp=average_oil_temp, safe_percentage=safe_percentage)
+        db.session.add(new)
+        db.session.commit()
+        return jsonify({"message": "success", "date": date})
+    else:
+        return jsonify({"message": "failed"})
+    
+@app.route('/check', methods=['POST'])
+def check():
+    date = request.form.get('date')
+    data = CustTemp.query.filter(CustTemp.date == date).all()
+    if data:
+        return jsonify({'message': 'success'})    
+    else:
+        return jsonify({'message': 'failed'})    
+
+@app.route('/make-pdf', methods=['POST'])
+def make_pdf():
+    date = request.form.get('date')
+    date_obj = datetime.strptime(date, "%Y-%m-%d")
+
+    formatted_date = date_obj.strftime("%d %B %Y")
+    data = CustTemp.query.filter(CustTemp.date == date).all()
+
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer, pagesize=A4)
+    p.setFont("Helvetica-Bold", 16)
+    p.drawString(60, 765, f"Daily Report {formatted_date}")
+
+    table_data = [['No', 'Nama', 'Suhu', 'Safe Status']]
+    for i, row in enumerate(data):
+        table_data.append([i+1, row.nama, str(row.average_temp) + '°C', str(row.safe_percentage) + '%'])
+
+    col_widths = [35, 280, 80, 80]
+    t = Table(table_data, colWidths=col_widths)
+    
+    style = [('BACKGROUND', (0, 0), (-1, 0), '#4472C4'),
+             ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+             ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+             ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+             ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+             ('FONT', (0, 0), (-1, -1), 'Helvetica', 13)]
+
+    t.setStyle(style)
+
+    cell_height = 20 
+    table_height = (len(data) + 1) * cell_height
+
+    y = 47
+    y2 = (table_height/20 - 1) * 2
+    table_y = 765 - 60 - table_height + (y-y2)
+    
+    t.wrapOn(p, 0, 0)
+    t.drawOn(p, 60, table_y)
+
+    p.save()
+    buffer.seek(0)
+
+    return send_file(buffer, as_attachment=True, download_name=f'Report.pdf')
+
+if __name__ == '__main__':
+    app.run(debug=True)
