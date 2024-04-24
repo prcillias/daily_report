e7125c65a229c6dae01c0cf772835960ab65a2a4
prcillias <pricilliasilfany22@gmail.com>
2024-04-24 08:51:43 +0700

    update unbalance count

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pandas as pd
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import and_
+from sqlalchemy import func
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -25,6 +26,20 @@ class CustTemp(db.Model):
     nama = db.Column(db.String(255))
     average_temp = db.Column(db.Float)
     safe_percentage = db.Column(db.Float)
+    
+    def __repr__(self):
+        return f'<{self.nama}>'
+    
+class CustUnbalance(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    date = db.Column(db.String(10))
+    nama = db.Column(db.String(255))
+    van = db.Column(db.Float)
+    vbn = db.Column(db.Float)
+    vcn = db.Column(db.Float)
+    vab = db.Column(db.Float)
+    vbc = db.Column(db.Float)
+    vca = db.Column(db.Float)
     unbalance_count = db.Column(db.Integer)
     
     def __repr__(self):
@@ -34,6 +49,11 @@ class CustTemp(db.Model):
 def home():
     data = CustTemp.query.all()
     return render_template('index.html', data=data)
+
+@app.route('/unbalance')
+def unbalance():
+    data = CustUnbalance.query.with_entities(CustUnbalance.nama).distinct().all()
+    return render_template('unbalance.html', data=data)
 
 @app.route('/get-data', methods=['GET'])
 def get_data():
@@ -46,8 +66,31 @@ def get_data():
             formatted_item = {
                 'nama': item.nama,
                 'average_temp': item.average_temp,
-                'safe_percentage': item.safe_percentage,
-                'unbalance_count': item.unbalance_count
+                'safe_percentage': item.safe_percentage
+            }
+            formatted_data.append(formatted_item)
+        return jsonify({"message": "success", "data": formatted_data})
+    else:
+        return jsonify({"message": "no data"})
+    
+@app.route('/get-data-voltage', methods=['GET'])
+def get_data_voltage():
+    date = request.args.get('date')
+    cust = request.args.get('cust')
+
+    data = CustUnbalance.query.filter(and_(CustUnbalance.date == date, CustUnbalance.nama == cust)).all()
+    formatted_data = []
+    if data:
+        for item in data:
+            formatted_item = {
+                'nama': item.nama,
+                'van': item.van,
+                'vbn': item.vbn,
+                'vcn': item.vcn,
+                'vab': item.vab,
+                'vbc': item.vbc,
+                'vca': item.vca,
+                'unbalance_count': item.unbalance_count,
             }
             formatted_data.append(formatted_item)
         return jsonify({"message": "success", "data": formatted_data})
@@ -65,7 +108,18 @@ def clear_data():
         return jsonify({"message": "success"})
     else:
         return jsonify({"message": "no data"}) 
-
+    
+@app.route('/clear-data-voltage', methods=['GET'])
+def clear_data_voltage():
+    date = request.args.get('date')
+    data = CustUnbalance.query.filter(CustUnbalance.date == date).all()
+    if data:
+        for d in data:
+            db.session.delete(d)
+        db.session.commit()
+        return jsonify({"message": "success"})
+    else:
+        return jsonify({"message": "no data"}) 
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -89,19 +143,49 @@ def upload():
         average_oil_temp = round(all_data['OilTemp'].mean(), 2)
         value_counts = all_data['Status'].value_counts()
         safe_percentage = round((value_counts.get("Safe", 0) / len(all_data)) * 100, 2)
-        diff = abs(df['Van'] - df['Vbn']) + abs(df['Van'] - df['Vcn']) + abs(df['Vbn'] - df['Vcn'])
-        unbalance_count = int((diff > 15).sum())
-        new = CustTemp(date=date, nama=nama, average_temp=average_oil_temp, safe_percentage=safe_percentage, unbalance_count=unbalance_count)
+        new = CustTemp(date=date, nama=nama, average_temp=average_oil_temp, safe_percentage=safe_percentage)
         db.session.add(new)
         db.session.commit()
         return jsonify({"message": "success", "date": date})
     else:
         return jsonify({"message": "failed"})
     
+@app.route('/upload_voltage', methods=['POST'])
+def upload_voltage():
+    nama = request.form.get('selectedCompany')
+    date = request.form.get('date')
+    uploaded_files = request.files.getlist('excel_files')
+    data = []
+
+    existing_data = CustUnbalance.query.filter_by(nama=nama, date=date).first()
+    if existing_data:
+        return jsonify({"message": "existed"})
+    
+    for index, excel_file in enumerate(uploaded_files):
+        if excel_file.filename != '':
+            df = pd.read_excel(excel_file, usecols="H,I,Q,R,Z,AA")
+            
+            for _, row in df.iterrows():
+                diff = abs(row['Van'] - row['Vbn']) + abs(row['Van'] - row['Vcn']) + abs(row['Vbn'] - row['Vcn'])
+                unbalance_count = int(diff > 15)
+                
+                new = CustUnbalance(date=date, nama=nama, van=row['Van'], vbn=row['Vbn'], vcn=row['Vcn'], vab=row['Vab'], vbc=row['Vbc'], vca=row['Vca'], unbalance_count=unbalance_count)
+                db.session.add(new)
+                db.session.commit()
+
+    return jsonify({"message": "success", "date": date})
+    
 @app.route('/check', methods=['POST'])
 def check():
+    type = request.form.get('type')
     date = request.form.get('date')
-    data = CustTemp.query.filter(CustTemp.date == date).all()
+    
+    if type == 't':
+        data = CustTemp.query.filter(CustTemp.date == date).all()
+    elif type == 'v':
+        cust = request.form.get('cust')
+        data = CustUnbalance.query.filter(and_(CustUnbalance.date == date, CustUnbalance.nama == cust)).all()
+        
     if data:
         return jsonify({'message': 'success'})    
     else:
@@ -120,11 +204,11 @@ def make_pdf():
     p.setFont("Helvetica-Bold", 16)
     p.drawString(60, 765, f"Daily Report {formatted_date}")
 
-    table_data = [['No', 'Nama', 'Suhu', 'Safe Status']]
+    table_data = [['No', 'Nama', 'Suhu', 'Safe Percentage']]
     for i, row in enumerate(data):
-        table_data.append([i+1, row.nama, str(row.average_temp) + '°C', str(row.safe_percentage) + '%', row.unbalance_count])
+        table_data.append([i+1, row.nama, str(row.average_temp) + '°C', str(row.safe_percentage) + '%'])
 
-    col_widths = [35, 195, 65, 80, 100]
+    col_widths = [35, 195, 122.5, 122.5]
     t = Table(table_data, colWidths=col_widths)
     
     style = [('BACKGROUND', (0, 0), (-1, 0), '#4472C4'),
@@ -146,6 +230,65 @@ def make_pdf():
     
     t.wrapOn(p, 0, 0)
     t.drawOn(p, 60, table_y)
+
+    p.save()
+    buffer.seek(0)
+
+    return send_file(buffer, as_attachment=True, download_name=f'Report.pdf')
+
+@app.route('/make-pdf-voltage', methods=['POST'])
+def make_pdf_voltage():
+    cust = request.form.get('cust')
+    date = request.form.get('date')
+    date_obj = datetime.strptime(date, "%Y-%m-%d")
+
+    formatted_date = date_obj.strftime("%d %B %Y")
+    data = CustUnbalance.query.filter(and_(CustUnbalance.date == date, CustUnbalance.nama == cust)).all()
+    total_unbalance_count = db.session.query(func.sum(CustUnbalance.unbalance_count)).filter(and_(CustUnbalance.date == date, CustUnbalance.nama == cust)).scalar()
+    
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer, pagesize=A4)
+    
+    chunk_size = 30
+    chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
+    
+    p.setFont("Helvetica-Bold", 16)
+    p.drawString(60, 765, f"{cust} Daily Report {formatted_date}")  
+    p.setFont("Helvetica", 14)
+    p.drawString(60, 745, f"Total Unbalanced: {total_unbalance_count}")  
+    
+    for chunk in chunks:
+        
+        table_data = [['Van', 'Vbn', 'Vcn', 'Vab', 'Vbc', 'Vca', 'Unbalance Count']]
+        for row in chunk:
+            table_data.append([row.van, row.vbn, row.vcn, row.vab, row.vbc, row.vca, row.unbalance_count])
+
+        col_widths = [60, 60, 60, 60, 60, 60, 115]
+        t = Table(table_data, colWidths=col_widths)
+        
+        style = [('BACKGROUND', (0, 0), (-1, 0), '#4472C4'),
+                ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+                ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                ('FONT', (0, 0), (-1, -1), 'Helvetica', 12)]
+
+        t.setStyle(style)
+
+        cell_height = 20 
+        table_height = (len(chunk) + 1) * cell_height
+
+        y = 0
+        y2 = 0
+        # y = 47
+        # y2 = (table_height/20 - 1) * 2
+        
+        table_y = 745 - 40 - table_height + (y-y2)
+        
+        t.wrapOn(p, 0, 0)
+        t.drawOn(p, 60, table_y)
+        p.showPage()
 
     p.save()
     buffer.seek(0)
